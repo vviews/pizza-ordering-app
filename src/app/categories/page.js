9e39fcf8b7d6a82f2b9e37334b6c68234e420388
vviews <88729789+vviews@users.noreply.github.com>
2024-02-08 14:57:25 +0700

    add key

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -91,14 +91,14 @@ export default function CategoriesPage (){
                 <h2 className="mt-8 text-sm text-gray-500">Edit category:</h2>
                 {categories?.length > 0 && categories.map((category) => (
                     <button 
+                    key={category.name}
                     onClick={() => {
                         setEditedCategory(category)
                         setCategoryName(category.name)
                     }}
-                    key={category.name}
                     className="bg-gray-200 rounded-xl p-2 px-4 flex gap-1 cursor-pointer mb-1"
                     >
-                        <span>{category.name}</span>
+                        {category.name}
                     </button>
                 ))}
             </div>
